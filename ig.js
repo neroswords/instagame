7ebf77635259d68c63d7f4da38e6363159671cd2
unknown <tanathep.min@gmail.com>
2020-04-12 04:01:47 +0700

    Try to use tag

--- ig.js
+++ ig.js
@@ -8,7 +8,8 @@ app.set("views", [path.join(__dirname, "views"),
                     path.join(__dirname, "views/Login_Form"),
                     path.join(__dirname, "views/Forget_password"),
                     path.join(__dirname, "views/Sign_Up"),
-                    path.join(__dirname, "views/Team")]);
+                    path.join(__dirname, "views/Team"),
+                    path.join(__dirname, "views/column")]);
 
 app.set("view engine", "ejs");
 
@@ -25,8 +26,8 @@ app.get("/team/create", function(req,res){
     res.render("c_team");
 });
 
-app.get("/Create/subjuect", function(req,res){
-    res.render("SignUp");
+app.get("/subject/create", function(req,res){
+    res.render("c_column");
 });
 
 app.listen(3000, function(req,res){
