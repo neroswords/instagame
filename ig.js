47aad4aedbf308e205d4c83dc985ad7a0de46760
unknown <tanathep.min@gmail.com>
2020-06-25 04:09:38 +0700

    try again

--- ig.js
+++ ig.js
@@ -24,7 +24,7 @@ const User = require("./models/user"),
 
 mongoose.set('useUnifiedTopology',true)   
 mongoose.set('useCreateIndex', true)
-mongoose.connect( process.env.IG_DATABASE || "mongodb://localhost/ig_db" , {useNewUrlParser: true, useUnifiedTopology: true}); // URI || 
+mongoose.connect( process.env.IG_DATABASE  || URI || "mongodb://localhost/ig_db" , {useNewUrlParser: true, useUnifiedTopology: true}); //  
 mongoose.set('useFindAndModify',false)
 
 app.use(methodOverride("_method"));
