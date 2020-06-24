722f7c2cf712e49444c270f6de018f1945eb268c
unknown <tanathep.min@gmail.com>
2020-06-25 06:04:42 +0700

    อีหนุ่ยย

--- routes/communication.js
+++ routes/communication.js
@@ -32,56 +32,14 @@ const express = require("express"),
     
     const upload = multer({storage : storage, fileFilter : imageFilter});
 
-// router.get("/", function(req,res){
-//     Tag.find({name : "Survival"}, function(err,foundTag){
-//         Communication.find({tags : {$in : foundTag }}, function(err,foundSurvival){
-//             var n_Survival = foundSurvival;
-//             Tag.find({name : "RPG"}, function(err,foundTag){
-//                 Communication.find({tags : {$in : foundTag }}, function(err,foundRPG){
-//                     var n_RPG = foundRPG;    
-//                     Tag.find({name : "Battle Royal"}, function(err,foundTag){
-//                         Communication.find({tags : {$in : foundTag }}, function(err,foundBattleR){
-//                             var n_Battle_Royal = foundBattleR;
-//                             Tag.find({name : "FPS"}, function(err,foundTag){
-//                                 Communication.find({tags : {$in : foundTag }}, function(err,foundFPS){
-//                                     var n_FPS = foundFPS;
-//                                     Tag.find({name : "MOBA"}, function(err,foundTag){
-//                                         Communication.find({tags : {$in : foundTag }}, function(err,foundMOBA){
-//                                             var n_MOBA = foundMOBA;
-//                                             Tag.find({name : "Fighting"}, function(err,foundTag){
-//                                                 Communication.find({tags : {$in : foundTag }}, function(err,foundFighting){
-//                                                     var n_Fighting = foundFighting;  
-//                                                     Tag.find({name : "Sports"}, function(err,foundTag){
-//                                                         Communication.find({tags : {$in : foundTag }}, function(err,foundSports){
-//                                                             var n_Sports = foundSports;
-//                                                             Tag.find({name : "Racing"}, function(err,foundTag){
-//                                                                 Communication.find({tags : {$in : foundTag }}, function(err,foundRacing){
-//                                                                     var n_Racing = foundRacing;
-//                                                                     res.render("column_game_type", {Battle_Royal : n_Battle_Royal,
-//                                                                                         FPS : n_FPS,
-//                                                                                         Fighting : n_Fighting,
-//                                                                                         MOBA : n_MOBA,
-//                                                                                         RPG : n_RPG,
-//                                                                                         Racing : n_Racing,
-//                                                                                         Sports : n_Sports,
-//                                                                                         Survival : n_Survival, 
-//                                                                                         moment: moment});
-//                                                                 }).sort({date : -1}).limit(2);
-//                                                             })    
-//                                                         }).sort({date : -1}).limit(2)
-//                                                     });
-//                                                 }).sort({date : -1}).limit(2);
-//                                             })    
-//                                         }).sort({date : -1}).limit(2);
-//                                     })  
-//                                 }).sort({date : -1}).limit(2);
-//                             })  
-//                         }).sort({date : -1}).limit(2);
-//                     })
-//                 }).sort({date : -1}).limit(2);
-//             })           
-//         }).sort({date : -1}).limit(2);
-//     })
+router.get("/", function(req,res){
+    Communication.find({}, function(error, someCommu){
+        if(error){
+            console.log(error);        
+        }else {
+            res.render("column",{Commu : someCommu, moment: moment});
+        }
+    }).sort({date : -1});
     // Communication.find({}, function(error,allCommu){
     //     if(error){
     //         console.log("Error to find commu database");
@@ -90,9 +48,9 @@ const express = require("express"),
     //         res.render("column", {Commu : allCommu, moment: moment});
     //     }
     // }).sort({date : -1})
-// });
+});
 
-router.get("/", function(req,res){
+router.get("/category", function(req,res){
     Communication.find({}, function(error, someCommu){
         if(error){
             console.log(error);        
